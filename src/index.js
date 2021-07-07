81ea86ec7d01370c415800f34091b9953f98f92e
Ezra Lim <ezralim1992@gmail.com>
2021-07-07 12:17:06 +0800

    Add initial calculator scaffolding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-export default function fifoCalculator() {
-  
+import InputError from './errors/input-error.js';
+
+let stockCostBuckets = [];
+
+function addStock({ quantity, costPerItem }) {
+  stockCostBuckets.push({ quantity, costPerItem });
+}
+
+function removeStock(quantity) {
+  let costs = [];
+  if (stockCostBuckets[0].quantity > quantity) {
+    stockCostBuckets[0].quantity -= quantity;
+    costs.push(stockCostBuckets[0].costPerItem);
+  } else if (stockCostBuckets[0].quantity === quantity) {
+    costs.push(stockCostBuckets[0].costPerItem);
+    stockCostBuckets.shift();
+  } else {
+    let remainingQuantity = quantity - stockCostBuckets[0].quantity;
+    costs.push(stockCostBuckets[0].costPerItem);
+    stockCostBuckets.shift();
+    costs.concat(removeStock(remainingQuantity));
+  }
+
+  return costs;
+}
+
+function isValidInput(stockMovement) {
+  return isNumeric(stockMovement.quantity) &&
+    isNumeric(stockMovement.costPerItem);
+}
+
+export default function fifoCalculator(stockSeries = []) {
+  stockSeries.forEach((stockMovement) => {
+    if (!isValidInput(stockMovement)) {
+      throw new InputError('Invalid input', stockMovement);
+    }
+
+    if (stockMovement.quantity > 0) {
+      addStock(stockMovement);
+    }
+
+    if (stockMovement.quantity < 0) {
+      removeStock(stockMovement.quantity);
+    }
+  })
 }
--- test/fifoCalculator.test.js
+++ test/fifoCalculator.test.js
@@ -1,3 +1,13 @@
-test('true', () => {
+import fifoCalculator from '../src/index.js'
+import InputError from '../src/errors/input-error.js'
+
+const inputArray = [
+  {
+    "quantity": 1.0,
+    "costPerItem": 10.0
+  }
+]
+
+test('fifoCalculator', () => {
   expect(true).toBe(true)
 })
